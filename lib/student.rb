01c37ce64c7131b2036282ad3933b935b48b509b
Mariam Haider <mfh.haider@gmail.com>
2020-04-07 22:38:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
-
+class Student < User
+  def first_name 
+    @first_name
+  end 
+  
+  def last_name
+    @last_name
+  end 
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(string)
+    @knowledge << string
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
 end
