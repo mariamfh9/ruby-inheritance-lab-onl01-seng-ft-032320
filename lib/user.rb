e522de7c97727b4d1f5356c944087f66bcf6aa50
Mariam Haider <mfh.haider@gmail.com>
2020-04-07 22:32:02 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,25 +1,7 @@
 class User 
   attr_accessor :first_name, :last_name
   
-  def first_name 
-    @first_name
-  end 
-  
-  def last_name
-    @last_name
-  end 
-  
-  def first_name=(first_name)
-    @first_name = first_name
-  end 
-  
-  def last_name=(last_name)
-    @last_name = last_name
-  end 
-  
-  def initialize
-    @knowledge = []
-  end 
+
   
 
 
