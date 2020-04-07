815279b3c314ed3d3e133fbfd41aefaf202b5035
Mariam Haider <mfh.haider@gmail.com>
2020-04-07 22:31:21 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -9,10 +9,16 @@ class User
     @last_name
   end 
   
-  def initialize(first_name, last_name)
-    @knowledge = []
+  def first_name=(first_name)
     @first_name = first_name
+  end 
+  
+  def last_name=(last_name)
     @last_name = last_name
+  end 
+  
+  def initialize
+    @knowledge = []
   end 
   
 
