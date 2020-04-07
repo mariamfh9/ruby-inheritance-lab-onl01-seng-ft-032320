d4bb0842f4be23f5824cbc60f48a1b7fed51ce3b
Mariam Haider <mfh.haider@gmail.com>
2020-04-07 22:33:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student > class User
-
+class Student < User
+  def first_name 
+    @first_name
+  end 
+  
+  def last_name
+    @last_name
+  end 
+  
+  def initialize
+    @knowledge = []
+  end 
 end
